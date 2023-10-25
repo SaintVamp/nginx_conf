2f6322ff82e0b144a67bc47410d9460d38d38808
sv-action <saint-wp@163.com>
2023-10-25 15:21:18 +0000

    merge gitee

--- update-nginx.sh
+++ update-nginx.sh
@@ -55,8 +55,10 @@ fi
 
 hostname=$(uci get system.@system[0].hostname)
 if [ "$hostname" = "R404" ]; then
+    echo "run in R404"
     curl -o /etc/nginx/nginx.conf https://gitee.com/saintvamp/nginx_conf/raw/master/nginx-404.conf
 elif [ "$hostname" = "R2804" ]; then
+    echo "run in R2804"
     curl -o /etc/nginx/nginx.conf https://gitee.com/saintvamp/nginx_conf/raw/master/nginx-2804.conf
 fi
 sleep 2
